28537e4f4593309e2677511e57db09c466ba06d3
fathimafahad123 <fathimasherinsalam99@gmail.com>
2023-08-03 18:52:39 +0530

    up

---
+++ main.js
@@ -0,0 +1,32 @@
+function calculateBMI() {
+    var height = document.getElementById("height").value;
+    var weight = document.getElementById("weight").value;
+
+    // Convert height to meters
+    Height = height / 100;
+
+    // Calculate BMI
+    var bmi = weight / (Height * Height);
+
+    // Display BMI
+    var res = document.getElementById("result")
+    res.innerHTML = bmi;
+    res.style.color = "dark-gray";
+    res.style.fontSize ="20px"
+    // res.style.body.marginTop ="12px"
+    // using if condition
+
+    if (bmi < 18.5) {
+        document.getElementById("result").innerHTML = `Underweight: ${bmi}`
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+        document.getElementById("result").innerHTML = `Normal weight: ${bmi}`
+    } else if (bmi >= 25 && bmi < 29.9) {
+        document.getElementById("result").innerHTML = `Overweight: ${bmi}`
+    } else if (bmi >= 29.9) {
+        document.getElementById("result").innerHTML = `Overweight above: ${bmi}`
+    }
+    else {
+        alert(`please enter a valid inputs`)
+    }
+}
+//   document.getElementById("calculate").addEventListener("click", calculateBMI);
